4e5210de0529c53fa2ef4ccb396f6edc49f3e6e8
Per von Zweigbergk <pvz@pvz.pp.se>
2023-08-27 19:27:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-plugin-mclag',
+    version='0.1',
+    description='Manage Multi-Chassis Link Aggregation Groups in Netbox (MC-LAG / MLAG / vPC / etc)',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)

b39507d130c04b20d835daa33ed1a17efbf9529b
Per von Zweigbergk <pvz@pvz.pp.se>
2024-05-24 13:41:27 +0200

    Bump version to 0.2.0

--- netbox_plugin_mclag/__init__.py
+++ netbox_plugin_mclag/__init__.py
@@ -5,7 +5,7 @@ class NetBoxMcLagConfig(PluginConfig):
     name = "netbox_plugin_mclag"
     verbose_name = "Multi-Chassis LAG"
     description = "Manage Multi-Chassis Link Aggregation Groups in Netbox (MC-LAG / MLAG / vPC / etc)"
-    version = "0.1.1"
+    version = "0.2.0"
     base_url = "mclag"
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-plugin-mclag',
-    version='0.1.1',
+    version='0.2.0',
     description='Manage Multi-Chassis Link Aggregation Groups in Netbox (MC-LAG / MLAG / vPC / etc)',
     install_requires=[],
     packages=find_packages(),

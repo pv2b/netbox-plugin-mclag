a17830025ed2b8af1bbcaba435cd1b9036885cde
VitMain <1895333+VitMain@users.noreply.github.com>
2025-03-21 13:53:46 -0500

    Fix bump to 0.2.1

--- netbox_plugin_mclag/__init__.py
+++ netbox_plugin_mclag/__init__.py
@@ -5,7 +5,7 @@ class NetBoxMcLagConfig(PluginConfig):
     name = "netbox_plugin_mclag"
     verbose_name = "Multi-Chassis LAG"
     description = "Manage Multi-Chassis Link Aggregation Groups in Netbox (MC-LAG / MLAG / vPC / etc)"
-    version = "0.2.0"
+    version = "0.2.1"
     base_url = "mclag"
 
 
